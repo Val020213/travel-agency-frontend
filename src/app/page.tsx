01715f40747cebb521d48b4abd958609e679059a
Osvaldo R.Moreno Prieto <osvaldo020213.com>
2024-02-06 11:20:31 -0500

    feat: example of tailwind and fetch todo: find bug controller and error set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
 "use client";
 import { useFetch } from "../hooks/useFetch";
+import { IconChevronRight } from "@tabler/icons-react";
 
 const UseFetchComponent = (url: string) => {
   const { data, error, loading } = useFetch(url);
   return (
-    <div>
-      <p>
-        {loading && "Loading..."}
-        {error && "Error"}
-        {data?.map((item: any) => (
-          <p key={item.id}>{item.title}</p>
-        ))}
-      </p>
+    <div className="flex flex-wrap gap-4 p-32">
+      {loading ? "Loading..." : null}
+      {!loading && error ? "Error" : null}
+      {data?.map((item: any) => (
+        <div
+          key={item.id}
+          className="flex flex-col p-8 w-72 shadow-xl bg-extends-light-blue-50 text-black rounded-lg gap-4"
+        >
+          <span className="text-2xl">{item.title}</span>
+          <span className="text-lg">{item.body}</span>
+          <span className="text-lg flex flex-row justify-start items-center">
+            {"Go"}
+            <IconChevronRight />
+          </span>
+        </div>
+      ))}
     </div>
   );
 };
--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -4,36 +4,29 @@ export const useFetch = (url: string) => {
   const [data, setData] = useState<any[]>([]);
   const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  const [controller, setController] = useState<AbortController>(
-    new AbortController()
-  );
 
   useEffect(() => {
-    setController(new AbortController());
+    const controller = new AbortController();
     setLoading(true);
     fetch(url, { signal: controller.signal })
       .then((response) => response.json())
-      .then((data) => {
-        setData(data);
+      .then((fetchedData) => {
+        console.log(fetchedData);
+        setData(fetchedData);
       })
-      .catch((error) => {
-        console.log(error);
-        setError(error);
+      .catch((fetchError) => {
+        console.log(fetchError);
+        setError(fetchError);
       })
       .finally(() => {
+        setError(null);
         setLoading(false);
       });
 
     return () => {
       controller.abort();
     };
-  }, [url, controller]);
+  }, [url]);
+
   return { data, error, loading };
 };
-
-// const handleCancelRequest = (controller: AbortController, setError: any) => {
-//   if (controller) {
-//     controller.abort();
-//     setError(new Error("Request canceled by user"));
-//   }
-// };
