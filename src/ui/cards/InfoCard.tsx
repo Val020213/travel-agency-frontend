a6fa64c6e5b00ebb9df2a1cf0e09aec68a830ba0
JoseAConcepcion <concepcijoseantonio@gmail.com>
2024-03-01 23:36:23 -0500

    fix: cardInfo responsiveness

--- src/ui/cards/InfoCard.tsx
+++ src/ui/cards/InfoCard.tsx
@@ -18,19 +18,24 @@ export const InfoCard = ({
         "rounded-[36px]",
         "shadow-xl",
         "bg-white dark:bg-extends-darker-blue-900",
-        " gap-9 p-9 pb-12",
-        "md:w-[700px] w-[300px]",
+        "gap-3 p-8 pb-8 md:gap-9 md:p-9 md:pb-12",
+        "w-[300px] md:w-[700px]",
       )}
     >
-      <h1 className="text-4xl font-medium text-gray-900 dark:text-white">{name}</h1>
-      <div className="flex w-full flex-col gap-4 *:flex *:flex-row *:gap-2 text-gray-500 dark:text-extends-darker-blue-200 text-lg">
+      <h1 className=" w-full mx-auto break-words text-xl text-center md:text-4xl font-medium text-gray-900 dark:text-white">{name}</h1>
+      <div className="flex w-full mx-auto break-words flex-col gap-4 *:flex *:flex-row *:gap-2 text-gray-500 dark:text-extends-darker-blue-200 text-base md:text-lg">
+        
         <div>
-          <IconMapPin />
+          <IconMapPin/>
+          <div className="w-full mx-auto">
           {address}
+          </div>
         </div>
-        <div>
+        <div >
           <IconBrandWhatsapp />
+          <div className="w-full mx-auto">
           {phone}
+          </div>
         </div>
       </div>
     </div>
